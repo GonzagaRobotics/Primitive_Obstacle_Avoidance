b679fb95ac99a1904a48ab6b69c1aea8bb26511f
Anicolazzo <anicolazzo@zagmail.gonzaga.edu>
2025-02-23 13:40:35 -0800

    d
    d

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_distance():
     while GPIO.input(echoPin) == 1:
         stop_time = time.time()  
     elapsed_time = stop_time - start_time
-    distance = (elapsed_time * .0343) / 2 
+    distance = (elapsed_time * 3430) / 2 
     return round(distance, 2)
 
 def send_serial_stop( ):
