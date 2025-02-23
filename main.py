8082ea2859023737f2bc8b12eb8198d111d7558d
Anicolazzo <anicolazzo@zagmail.gonzaga.edu>
2025-02-23 13:17:50 -0800

    t
    t

--- main.py
+++ main.py
@@ -27,7 +27,9 @@ def get_distance():
     return round(distance, 2)
 
 def send_serial_stop( ):
-    ser.write(f"FB:100, LR:100, EN:0")
+   # ser.write(f"FB:100, LR:100, EN:0")
+    print(f"FB:100, LR:100, EN:0")
+
     return None
 
 def main():
